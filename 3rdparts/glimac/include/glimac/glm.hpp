ff0682f135e28e90cf0ce343ad03197cc510e6b4
lujlujluj <jules.tantot@gmail.com>
2015-12-15 15:53:32 +0100

    0.1 – Setup projet

---
+++ 3rdparts/glimac/include/glimac/glm.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#define GLM_FORCE_RADIANS
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include <gtc/constants.hpp>
+#include <gtx/io.hpp>
+#include <gtc/random.hpp>
